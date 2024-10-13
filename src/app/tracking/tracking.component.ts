4dbe367e278dd25ec0ecf50b2928fb5be0816bd3
Naveed Aheer <naveedaheerpc@gmail.com>
2024-10-13 12:07:58 +0500

    Created tracking UI

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TrackingStatusStepBarComponent } from "./tracking-status-step-bar/tracking-status-step-bar.component";
+import { TrackingEventsComponent } from "./tracking-events/tracking-events.component";
 
 @Component({
   selector: 'app-tracking',
   standalone: true,
-  imports: [],
+  imports: [TrackingStatusStepBarComponent, TrackingEventsComponent],
   templateUrl: './tracking.component.html',
   styleUrl: './tracking.component.scss'
 })
