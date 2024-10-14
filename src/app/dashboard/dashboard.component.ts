75880dcb8842a7fa43222de1fd51e1c1083781ea
Naveed Aheer <naveedaheerpc@gmail.com>
2024-10-14 16:42:09 +0500

    Initialized sidebar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { ShipmentsComponent } from './shipments/shipments.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../core/components/header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from '../core/components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ShipmentsComponent, RouterOutlet, HeaderComponent],
+  imports: [ShipmentsComponent, RouterOutlet, HeaderComponent, MatSidenavModule, SidenavComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
