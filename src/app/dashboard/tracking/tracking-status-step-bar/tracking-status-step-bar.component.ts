438b0b8f66983c317651ab9d2fa623e49d1e34db
Naveed Aheer <naveedaheerpc@gmail.com>
2024-10-14 20:27:49 +0500

    Created stepper for tracking status

--- src/app/dashboard/tracking/tracking-status-step-bar/tracking-status-step-bar.component.ts
+++ src/app/dashboard/tracking/tracking-status-step-bar/tracking-status-step-bar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-tracking-status-step-bar',
   standalone: true,
-  imports: [],
+  imports: [MatStepperModule, MatIconModule],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
   templateUrl: './tracking-status-step-bar.component.html',
-  styleUrl: './tracking-status-step-bar.component.scss'
+  styleUrl: './tracking-status-step-bar.component.scss',
 })
 export class TrackingStatusStepBarComponent {
+  @Input() currentStatus!: string | undefined;
+  trackingStatusList: string[] = [
+    'Documentation Prepared',
+    'Booking Arranged',
+    'Receipt of Goods',
+    'In Transit',
+    'Arrived at Destination',
+    'Out for Delivery',
+    'Delivered',
+  ];
+
+  isCompleted(index: number): boolean {
+    const currentIndex = this.trackingStatusList.findIndex((status: string) =>
+      this.normalizeStatus(status) === this.normalizeStatus(this.currentStatus || '')
+    );
+    console.log("currentIndex ", currentIndex )
+    return currentIndex !== -1 && index < currentIndex;
+  }
+
+  normalizeStatus(status: string): string {
+    // Remove spaces from start, end, and middle
+    return status.replace(/\s+/g, '').toLowerCase();
+  }
 
 }
